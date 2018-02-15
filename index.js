d6ecaa7dda3bf6e9a04ae2b4ae61dc58417bd41c
Jerry P <jpull@jpull.com>
2018-02-15 20:57:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 if (!cardNumber) {
-  console.log(`Sorry, we don't have a credit card on file for you.`);
+  console.log("Sorry, we don't have a credit card on file for you.");
   return false;
 }
