88535f5e0cd9658f076b90328b28cc52f7a1f856
Jerry P <jpull@jpull.com>
2018-02-15 20:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function removeFromCart(item) {
         if (itemName === item) return(cart.splice(i,1));
       }
     }
-    return "That item is not in your cart.";
+    return('That item is not in your cart.');
 }
 
 function placeOrder(cardNumber) {
